c26c69663ea6442323892ebd7ccd7b3331a1d466
jkjkddd233 <13690065910@163.com>
2026-08-06 14:23:05 +0800

    Site updated: 2026-08-06 14:23:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/29/修复网易云音乐云盘周杰伦歌曲/"];function toRandomPost(){
+var posts=["2025/07/29/修复网易云音乐云盘周杰伦歌曲/","2026/08/06/关于部署twikoo时使用MongoDB遇到的问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
